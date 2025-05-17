c9f5e504390313c805772af11b2a1637edd18cce
Gihanwlf <pgihan29@gmail.com>
2025-05-17 19:35:30 +0530

    Initial commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { BookA, Calendar, Send } from 'lucide-react';
+import {  Calendar, Send } from 'lucide-react';
 
 interface HeaderProps {
   activeSection: string;
